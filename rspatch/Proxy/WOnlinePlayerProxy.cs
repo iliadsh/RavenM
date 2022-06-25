798dbfdb3e8d0273a6dd3226dfbf23ecc5808bfb
EaglePancake <eaglepancake@gmail.com>
2022-06-25 20:02:41 +0300

    Exposed send chat message to RS

--- rspatch/Proxy/WOnlinePlayerProxy.cs
+++ rspatch/Proxy/WOnlinePlayerProxy.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Lua.Proxy;
 using MoonSharp.Interpreter;
 using RavenM.RSPatch.Wrapper;
+using Steamworks;
+using UnityEngine;
 
 namespace RavenM.RSPatch.Proxy
 {
@@ -23,6 +26,28 @@ namespace RavenM.RSPatch.Proxy
                 return WOnlinePlayer.GetOwnGUID();
             }
         }
+
+        public static void SendChatMessage(string input, bool global)
+        {
+            IngameNetManager.instance.PushChatMessage(ActorManager.instance.player.name, input, global, GameManager.PlayerTeam());
+
+            using MemoryStream memoryStream = new MemoryStream();
+            var chatPacket = new ChatPacket
+            {
+                Id = ActorManager.instance.player.GetComponent<GuidComponent>().guid,
+                Message = input,
+                TeamOnly = !global,
+            };
+
+            using (var writer = new ProtocolWriter(memoryStream))
+            {
+                writer.Write(chatPacket);
+            }
+            byte[] data = memoryStream.ToArray();
+
+            IngameNetManager.instance.SendPacketToServer(data, PacketType.Chat, Constants.k_nSteamNetworkingSend_Reliable);
+        }
+
         [MoonSharpHidden]
         public object GetValue()
         {
