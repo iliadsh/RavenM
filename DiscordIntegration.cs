e29bf5b195efa8143d4a884fe279f5c6e6a14f58
Jorgegzz <A01571510@tec.mx>
2022-08-31 23:16:31 -0500

    Squashed Commits: Discord Integration

---
+++ DiscordIntegration.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Collections;
+using RavenM.DiscordGameSDK;
+using Steamworks;
+using UnityEngine;
+
+namespace RavenM
+{
+    public class DiscordIntegration : MonoBehaviour
+    {
+        public static DiscordIntegration instance;
+        
+        public Discord Discord;
+
+        public long discordClientID = 1007054793220571247;
+
+        public long startSessionTime;
+
+        private ActivityManager _activityManager;
+        private void Start()
+        {
+            Environment.SetEnvironmentVariable("DISCORD_INSTANCE_ID", "1007054793220571247");
+            Discord = new Discord(long.Parse(Environment.GetEnvironmentVariable("DISCORD_INSTANCE_ID")!), (UInt64) CreateFlags.Default);
+            Plugin.logger.LogWarning("Discord Instance created");
+            startSessionTime = ((DateTimeOffset) DateTime.Now).ToUnixTimeSeconds();
+            
+            _activityManager = Discord.GetActivityManager();
+            
+            StartCoroutine(StartActivities());
+            
+            _activityManager.OnActivityJoin += secret =>
+            {
+                secret = secret.Replace("_join", "");
+                
+                Plugin.logger.LogInfo($"OnJoin {secret}");
+                var LobbyID = new CSteamID(ulong.Parse(secret));
+
+                if (_isInGame)
+                {
+                    GameManager.ReturnToMenu();
+                }
+                
+                SteamMatchmaking.JoinLobby(LobbyID);
+                LobbySystem.instance.InLobby = true;
+                LobbySystem.instance.IsLobbyOwner = false;
+                LobbySystem.instance.LobbyDataReady = false;
+            };
+            
+            _activityManager.OnActivityJoinRequest += (ref User user) =>
+            {
+                // The Ask to join Button Doesnt even work rn (Discord's fault) try the right click Ask to join button instead
+                Plugin.logger.LogInfo($"OnJoinRequest {user.Username} {user.Id}");
+            };
+        }
+        
+        IEnumerator StartActivities()
+        {
+            UpdateActivity(Discord, Activities.InitialActivity);
+            yield return new WaitUntil(GameManager.IsInMainMenu);
+            UpdateActivity(Discord, Activities.InMenu);
+        }
+        
+        // Private Variables that makes me question my coding skills
+        private TimedAction _timer = new(5f);
+        
+        private string _gameMode = "Insert Game Mode";
+        private void FixedUpdate()
+        {
+           Discord.RunCallbacks();
+           
+           if (_timer.TrueDone())
+           {
+               ChangeActivityDynamically();
+
+               _timer.Start();
+           }
+        }
+
+        private bool _isInGame;
+        private bool _isInLobby;
+
+        void ChangeActivityDynamically()
+        {
+            if (GameManager.instance == null) { return; }
+
+            _isInGame = GameManager.instance.ingame;
+            _isInLobby = LobbySystem.instance.InLobby;
+
+
+            if (_isInGame && !_isInLobby)
+            {
+                var dropdown = InstantActionMaps.instance.gameModeDropdown;
+                _gameMode = dropdown.options[dropdown.value].text;
+                UpdateActivity(Discord, Activities.InSinglePlayerGame, true ,_gameMode);
+            }
+            else if (_isInLobby)
+            {
+                int currentLobbyMembers = SteamMatchmaking.GetNumLobbyMembers(LobbySystem.instance.ActualLobbyID);
+                int currentLobbyMemberCap = SteamMatchmaking.GetLobbyMemberLimit(LobbySystem.instance.ActualLobbyID);
+
+                if (!_isInGame) // Waiting in Lobby
+                {
+                    var dropdown = InstantActionMaps.instance.gameModeDropdown;
+                    _gameMode = dropdown.options[dropdown.value].text;
+                    UpdateActivity(Discord, Activities.InLobby, false ,_gameMode, currentLobbyMembers, currentLobbyMemberCap, LobbySystem.instance.ActualLobbyID.ToString());
+                }
+                else // Playing in a Lobby
+                {
+                    UpdateActivity(Discord, Activities.InLobby, true ,_gameMode, currentLobbyMembers, currentLobbyMemberCap, LobbySystem.instance.ActualLobbyID.ToString());
+                }
+            }
+            else // Left the lobby
+            {
+                UpdateActivity(Discord, Activities.InMenu);
+            }
+        }
+        
+        public void UpdateActivity(Discord discord, Activities activity, bool inGame = false, string gameMode = "None", int currentPlayers = 1, int maxPlayers = 2, string lobbyID = "None")
+        {
+            var activityManager = discord.GetActivityManager();
+            var activityPresence = new Activity();
+            
+            switch (activity)
+            {
+                case Activities.InitialActivity:
+                    activityPresence = new Activity()
+                    {
+                        State = "Just Started Playing",
+                        Assets =
+                        {
+                            LargeImage = "rfimg_1_",
+                            LargeText = "RavenM",
+                        },
+                        Instance = true,
+                    };
+                    break;
+                case Activities.InMenu:
+                    activityPresence = new Activity()
+                    {
+                        State = "Waiting In Menu",
+                        Assets =
+                        {
+                            LargeImage = "rfimg_1_",
+                            LargeText = "RavenM",
+                        },
+                        Instance = true,
+                    };
+                    break;
+                case Activities.InLobby:
+                    var state = inGame ? "Plating Multiplayer" : "Waiting In Lobby";
+                    activityPresence = new Activity()
+                    {
+                        State = state,
+                        Details = $"Game Mode: {gameMode}",
+                        Timestamps =
+                        {
+                            Start = startSessionTime,
+                        },
+                        Assets =
+                        {
+                            LargeImage = "rfimg_1_",
+                            LargeText = "RavenM",
+                        },
+                        Party = {
+                            Id = lobbyID,
+                            Size = {
+                                CurrentSize = currentPlayers,
+                                MaxSize = maxPlayers,
+                            },
+                        },
+                        Secrets =
+                        {
+                            Join = lobbyID + "_join",
+                        },
+                        Instance = true,
+                    };
+                    break;
+                case Activities.InSinglePlayerGame:
+                    activityPresence = new Activity()
+                    {
+                        State = "Playing Singleplayer",
+                        Timestamps =
+                        {
+                            Start = startSessionTime,
+                        },
+                        Assets =
+                        {
+                            LargeImage = "rfimg_1_",
+                            LargeText = "RavenM",
+                        },
+                        Instance = true,
+                    };
+                    break;
+                    
+               
+            }
+            activityManager.UpdateActivity(activityPresence, result =>
+            {
+                Plugin.logger.LogInfo($"Update Discord Activity {result}");
+            });
+        }
+
+        public enum Activities
+        {
+            InitialActivity,
+            InMenu,
+            InLobby,
+            InSinglePlayerGame,
+        }
+    }
+}
