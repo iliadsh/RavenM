c127984f1d628673de519d1ee502c0eca9e3b5d0
Hiteke <38786753+Hiteke@users.noreply.github.com>
2022-11-22 09:31:30 +0900

    Make download from github actions (#26)
    * Update main.yml
    
    * Update Plugin.cs
    
    * Update Plugin.cs
    
    * Update Plugin.cs
    
    * Update Plugin.cs
    
    * Update Plugin.cs

--- RavenM.Updater/Plugin.cs
+++ RavenM.Updater/Plugin.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using System.Reflection;
 using System;
 using System.IO;
@@ -6,7 +6,7 @@ using System.IO.Compression;
 using SimpleJSON;
 using System.Net;
 
-namespace RavenM.Updater 
+namespace RavenM.Updater
 {
     /// <summary>
     /// Unfortunately, BepInEx loads plugin .dll(s) by file path rather than loading them into memory first.
@@ -17,13 +17,37 @@ namespace RavenM.Updater
     class Plugin : BaseUnityPlugin
     {
         static readonly string PLUGIN_DIR = "BepInEx/plugins/";
+        static readonly string DEV_DOWNLOAD_URL = "https://nightly.link/iliadsh/RavenM/workflows/main/master/RavenM.zip";
+
+        enum UpdateChannel
+        {
+            Release,
+            Dev
+        }
+
+        private Stream MakeRequest(string url)
+        {
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            req.Accept = "application/vnd.github+json";
+            req.UserAgent = "Def-Not-RavenM";
+            req.Timeout = 5000;
+
+            return req.GetResponse().GetResponseStream();
+        }
 
         private void Awake()
         {
-            var force_current_version = Config.Bind("General", 
+            var force_current_version = Config.Bind("General",
                                                     "Force Disable Updates",
                                                     false,
                                                     "If you're using a potentially outdated version and know what you're doing, this will disabled the updater.");
+
+            var update_channel = Config.Bind("General",
+                                             "Update Channel",
+                                             UpdateChannel.Release);
+
+            var current_update_channel = (UpdateChannel)Enum.Parse(typeof(UpdateChannel), PlayerPrefs.GetString("UpdateChannel", "Release"), true);
 
             if (force_current_version.Value)
             {
@@ -33,51 +57,70 @@ namespace RavenM.Updater
 
             Logger.LogInfo("Checking for updates...");
 
-            var mod_version = AssemblyName.GetAssemblyName(PLUGIN_DIR + "RavenM.dll").Version;
+            DateTime mod_creation_time = DateTime.MinValue;
+            string time;
+            string download_url;
 
-            var req = (HttpWebRequest)WebRequest.Create("https://api.github.com/repos/iliadsh/RavenM/releases");
-            req.Method = "GET";
-            req.Accept = "text / html,application / xhtml + xml,application / xml; q = 0.9,image / webp,image / apng,*/*;q=0.8;";
-            req.UserAgent = "Def-Not-RavenM";
-            req.Timeout = 5000;
-            using var response = new StreamReader(req.GetResponse().GetResponseStream());
-
-            var json = JSON.Parse(response.ReadToEnd());
-
-            foreach (var kv in json.AsArray)
+            var mod_file = PLUGIN_DIR + "RavenM.dll";
+            if (File.Exists(mod_file))
             {
-                var release = kv.Value;
-                var tag = release["tag_name"];
-                if (Version.TryParse(tag, out Version release_version))
+                mod_creation_time = File.GetCreationTime(mod_file)
+            }
+
+            if (update_channel.Value == UpdateChannel.Release)
+            {
+                var response = MakeRequest("https://api.github.com/repos/iliadsh/RavenM/releases/latest");
+                var json = JSON.Parse(new StreamReader(response).ReadToEnd());
+                var asset = json["assets"][0];
+                time = asset["updated_at"];
+                download_url = asset["browser_download_url"];
+            }
+            else if (update_channel.Value == UpdateChannel.Dev)
+            {
+                var response = MakeRequest("https://api.github.com/repos/iliadsh/RavenM/actions/artifacts?name=RavenM&per_page=1");
+                var json = JSON.Parse(new StreamReader(response).ReadToEnd());
+                var artifact = json["artifacts"][0];
+                time = artifact["updated_at"];
+                download_url = DEV_DOWNLOAD_URL;
+            }
+            else
+            {
+                Logger.LogError($"Update channel {update_channel.Value} not found");
+                return;
+            }
+
+            DateTime upload_time;
+            if (DateTime.TryParse(time, out upload_time))
+            {
+                if ((current_update_channel == update_channel.Value && upload_time > mod_creation_time) || current_update_channel != update_channel.Value)
                 {
-                    if (release_version > mod_version)
+                    Logger.LogWarning($"Newer version found: {time}. Downloading...");
+
+                    var zip = MakeRequest(download_url);
+
+                    Logger.LogWarning("Downloaded new version. Updating...");
+
+                    var archive = new ZipArchive(zip, ZipArchiveMode.Read);
+                    foreach (var s in archive.Entries)
                     {
-                        Logger.LogWarning($"Newer version found: {tag}. Downloading...");
-
-                        var download_url = release["assets"][0]["browser_download_url"];
-                        using var client = new WebClient();
-                        using var zip = new MemoryStream(client.DownloadData(download_url));
-
-                        Logger.LogWarning("Downloaded new version. Updating...");
-
-                        using var archive = new ZipArchive(zip, ZipArchiveMode.Read);
-                        foreach (var s in archive.Entries)
+                        // We can't update the updater, since it's already locked by BepInEx.
+                        if (s.Name != "RavenM.Updater.dll")
                         {
-                            // We can't update the updater, since it's already locked by BepInEx.
-                            if (s.Name != "RavenM.Updater.dll")
+                            try
                             {
-                                try
-                                {
-                                    s.ExtractToFile(PLUGIN_DIR + s.Name, true);
-                                }
-                                catch (Exception e)
-                                {
-                                    Logger.LogError($"Failed to update file {s.Name}. {e}");
-                                }
+                                s.ExtractToFile(PLUGIN_DIR + s.Name, true);
+                                File.SetCreationTime(PLUGIN_DIR + s.Name, upload_time);
+                            }
+                            catch (Exception e)
+                            {
+                                Logger.LogError($"Failed to update file {s.Name}. {e}");
                             }
                         }
-                        break;
                     }
+
+                    PlayerPrefs.SetString("UpdateChannel", update_channel.Value.ToString());
+                    PlayerPrefs.Save();
+
                 }
             }
 
